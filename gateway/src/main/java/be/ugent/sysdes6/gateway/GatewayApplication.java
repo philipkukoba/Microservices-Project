0d853560e45e36c15dc42f75f2b8a7ca04b42516
pacalleb <paulien.callebaut@ugent.be>
2020-12-21 11:23:23 +0100

    gateway

--- gateway/src/main/java/be/ugent/sysdes6/gateway/GatewayApplication.java
+++ gateway/src/main/java/be/ugent/sysdes6/gateway/GatewayApplication.java
@@ -17,15 +17,15 @@ public class GatewayApplication {
 	@Bean
 	public RouteLocator customRouteLocator(RouteLocatorBuilder builder){
 		return builder.routes().
-				route(r->r.host("*").and().path("/voorraad/**").uri("http:://localhost:8080/api/voorraad")).
-				route(r->r.host("*").and().path("/catalogus/**").uri("http:://localhost:8080/api/catalogus")).
-				route(r->r.host("*").and().path("/afval/**").uri("http:://localhost:8082/api/order/afval")).
-				route(r->r.host("*").and().path("/gebruikers/**").uri("http:://localhost:3000/api/gebruikers")).
-				route(r->r.host("*").and().path("/bestellingen/**").uri("http:://localhost:8081/api/bestellingen")).
-				route(r->r.host("*").and().path("/statistieken/**").uri("http:://localhost:8081/api/statistieken")).
-				route(r->r.host("*").and().path("/koelcellen/**").uri("http:://localhost:8000/api/koelcellen")).
-				route(r->r.host("*").and().path("/ticket/**").uri("http:://localhost:3002/api/ticket")).
-				route(r->r.host("*").and().path("/boekhoud/**").uri("http:://localhost:3001/api/boekhoud")).
+				route(r->r.host("*").and().path("/api/voorraad/**").uri("http://localhost:8080/api/voorraad/")).
+				route(r->r.host("*").and().path("/api/catalogus/**").uri("http://localhost:8080/api/catalogus/")).
+				route(r->r.host("*").and().path("/api/afval/**").uri("http://localhost:8082/api/order/afval/")).
+				route(r->r.host("*").and().path("/api/gebruikers/**").uri("http://localhost:3000/api/gebruikers/")).
+				route(r->r.host("*").and().path("/api/bestellingen/**").uri("http://localhost:8081/api/bestellingen/")).
+				route(r->r.host("*").and().path("/api/statistieken/**").uri("http://localhost:8081/api/statistieken/")).
+				route(r->r.host("*").and().path("/api/koelcellen/**").uri("http://localhost:8000/api/koelcellen/")).
+				route(r->r.host("*").and().path("/api/ticket/**").uri("http://localhost:3002/api/ticket/")).
+				route(r->r.host("*").and().path("/api/boekhoud/**").uri("http://localhost:3001/api/boekhoud/")).
 				build();
 	}
 }
