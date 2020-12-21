bd9394028c556f494f2c6b82b25c89343bd68c06
gpveeckh <gauthier.vaneeckhoutte@ugent.be>
2020-12-21 20:51:01 +0100

    cross origin

--- medicijnen/src/main/java/be/ugent/systemdesign/group6/medicijnen/API/REST/CatalogusController.java
+++ medicijnen/src/main/java/be/ugent/systemdesign/group6/medicijnen/API/REST/CatalogusController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/api/catalogus")
+@CrossOrigin(origins="*")
 public class CatalogusController {
 
     @Autowired
