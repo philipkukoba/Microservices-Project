5655b58904750cb89a78b1b758273146b5ae4f50
gpveeckh <gauthier.vaneeckhoutte@ugent.be>
2020-12-22 13:00:05 +0100

    Yaml

--- gateway/src/main/java/be/ugent/sysdes6/gateway/GatewayApplication.java
+++ gateway/src/main/java/be/ugent/sysdes6/gateway/GatewayApplication.java
@@ -1,5 +1,6 @@
 package be.ugent.sysdes6.gateway;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
@@ -13,19 +14,40 @@ public class GatewayApplication {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
 
+	@Value("${medicijnen}")
+	private String medicijnen = "medicijnen:8080";
+
+	@Value("${order}")
+	private String order = "order:2222";
+
+	@Value("${gebruikers}")
+	private String gebruikers = "gebruikers:8080";
+
+	@Value("${bestellingen}")
+	private String bestellingen = "bestellingen:8080";
+
+	@Value("${koelcelmonitor}")
+	private String koelcelmonitor = "koelcelmonitor:8000";
+
+	@Value("${ticketdienst}")
+	private String ticketdienst = "ticketdienst:3000";
+
+	@Value("${boekhoudsdienst}")
+	private String boekhoudsdienst = "boekhoudsdienst:3000";
 
 	@Bean
 	public RouteLocator customRouteLocator(RouteLocatorBuilder builder){
+		System.out.println(medicijnen);
 		return builder.routes().
-				route(r->r.host("*").and().path("/api/voorraad/**").uri("http://medicijnen:8080/api/voorraad/")).
-				route(r->r.host("*").and().path("/api/catalogus/**").uri("http://medicijnen:8080/api/catalogus/")).
-				route(r->r.host("*").and().path("/api/order/afval/**").uri("http://order:2222/api/order/afval/")).
-				route(r->r.host("*").and().path("/api/gebruikers/**").uri("http://gebruikers:3000/api/gebruikers/")).
-				route(r->r.host("*").and().path("/api/bestellingen/**").uri("http://bestellingen:8080/api/bestellingen/")).
-				route(r->r.host("*").and().path("/api/statistieken/**").uri("http://bestellingen:8080/api/statistieken/")).
-				route(r->r.host("*").and().path("/api/koelcellen/**").uri("http://koelcelmonitor:8000/api/koelcellen/")).
-				route(r->r.host("*").and().path("/api/ticket/**").uri("http://ticketdienst:3000/api/ticket/")).
-				route(r->r.host("*").and().path("/api/boekhoud/**").uri("http://boekhoudsdienst:3000/api/boekhoud/")).
+				route(r->r.host("*").and().path("/api/voorraad/**").uri("http://"+medicijnen+"/api/voorraad/")).
+				route(r->r.host("*").and().path("/api/catalogus/**").uri("http://"+medicijnen+"/api/catalogus/")).
+				route(r->r.host("*").and().path("/api/order/afval/**").uri("http://"+order+"/api/order/afval/")).
+				route(r->r.host("*").and().path("/api/gebruikers/**").uri("http://"+gebruikers+"/api/gebruikers/")).
+				route(r->r.host("*").and().path("/api/bestellingen/**").uri("http://"+bestellingen+"/api/bestellingen/")).
+				route(r->r.host("*").and().path("/api/statistieken/**").uri("http://"+bestellingen+"/api/statistieken/")).
+				route(r->r.host("*").and().path("/api/koelcellen/**").uri("http://"+koelcelmonitor+"/api/koelcellen/")).
+				route(r->r.host("*").and().path("/api/ticket/**").uri("http://"+ticketdienst+"/api/ticket/")).
+				route(r->r.host("*").and().path("/api/boekhoud/**").uri("http://"+boekhoudsdienst+"/api/boekhoud/")).
 				build();
 	}
 }
