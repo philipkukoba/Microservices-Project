ef960735f4dc8b54beab53ee513da4e35389d2ff
bdbaer <Brecht.DeBaer@UGent.be>
2020-12-23 10:40:59 +0100

    CORS statistieken

--- bestellingen/src/main/java/be/ugent/systemdesign/group6/bestellingen/API/rest/StatistiekenController.java
+++ bestellingen/src/main/java/be/ugent/systemdesign/group6/bestellingen/API/rest/StatistiekenController.java
@@ -2,6 +2,7 @@ package be.ugent.systemdesign.group6.bestellingen.API.rest;
 
 import be.ugent.systemdesign.group6.bestellingen.application.CQRS.StatistiekenQuery;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("api/statistieken")
+@CrossOrigin(origins="*")
 public class StatistiekenController {
 
     @Autowired
